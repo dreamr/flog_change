e470b6fcb84677dd6f676cda187ce2815ce615df
Dreamr OKelly <dreamr.okelly@gmail.com>
2013-08-05 02:38:33 -0700

    basic erb html!

--- lib/flog_change/graph_generator.rb
+++ lib/flog_change/graph_generator.rb
@@ -1,9 +1,13 @@
+require 'erb'
 require 'flog_change'
 
 module FlogChange
   class GraphGenerator
-    def self.generate
-      
+    def self.generate(sample)
+      content   = File.read("./erb/index.html.erb")
+      @sample   = sample
+      renderer  = ERB.new(content)
+      File.open("./tmp/index.html", "w") {|f| f.puts(renderer.result(binding)) }
     end
   end
 end
--- test/flog_change/graph_generator_test.rb
+++ test/flog_change/graph_generator_test.rb
@@ -1,5 +1,8 @@
 require 'minitest_helper'
 require 'flog_change/graph_generator'
+require 'flog_change/sample'
+
+require 'nokogiri'
 
 describe FlogChange::GraphGenerator do
   subject { FlogChange::GraphGenerator }
@@ -7,10 +10,27 @@ describe FlogChange::GraphGenerator do
     # stub out the sample directory
     #   so we dont blow away real data
     FlogChange::App.stubs(:sample_dir).returns("./test/tmp/*.sample")
-
+    
+    # copy in sample files
+    `cp ./test/fixtures/samples/*.sample ./test/tmp/`
   end
+  
   describe ".generate" do
+    before do
+      `rm ./tmp/index.html`
+      file = Dir["./test/tmp/*.sample"][0]
+      subject.generate(FlogChange::Sample.new(file))
+      @content = File.open("./tmp/index.html") {|f| f.read}
+    end
 
+    it "must generate html file" do
+      `ls ./tmp/index.html`.must_equal "./tmp/index.html\n"
+    end
+
+    it "must include the sample score" do
+      node = Nokogiri::XML(@content)
+      node.css('div#sample_score').text.must_equal("4939.0")
+    end
   end
 end
 
