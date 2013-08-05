df94e3ba2b8543ebd29e26b9966fe12cc68b50a1
Dreamr OKelly <dreamr.okelly@gmail.com>
2013-08-05 02:00:47 -0700

    no longer writes to ./samples for tests

--- test/flog_change_test.rb
+++ test/flog_change_test.rb
@@ -6,12 +6,16 @@ describe FlogChange::App do
   describe ".samples" do
     let(:time) {Time.now.to_i}
     before do
+      # stub out the sample directory
+      #   so we dont blow away real data
+      FlogChange::App.stubs(:sample_dir).returns("./test/tmp/*.sample")
+
       # create a sample
-      `touch ./samples/#{time}.sample`
+      `touch ./test/tmp/#{time}.sample`
     end
 
     it "will return the sample file paths as an array" do
-      subject.samples.must_include "./samples/#{time}.sample"
+      subject.samples.must_include "./test/tmp/#{time}.sample"
     end
   end
 end
